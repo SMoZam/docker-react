0b9aa734c3536c52b389037f54e162cb8a38f11b
Zamama <sm.zamama@gmail.com>
2019-08-21 18:21:50 +0200

    test if it still running well

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to docker file reload.
+          Edit <code>src/App.js</code> and now it should be working really fine
         </p>
         <a
           className="App-link"
